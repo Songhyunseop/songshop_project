ffe7ec8b75e8b61255fea2337339bf49a47c9b5e
songhyunseop <gustjq99455352@gmail.com>
2024-07-14 07:23:44 +0900

    feat: react-select 컴포넌트, react-hook-form 연동 및 formData 핸들링 구현

--- src/components/commons/parts/select/index.tsx
+++ src/components/commons/parts/select/index.tsx
@@ -1,17 +1,33 @@
+import { Controller, useFormContext } from 'react-hook-form';
 import * as S from './styles';
 
-function CustomSelect({ subRef, register, ...rest }) {
+function CustomSelect({ register, subRef, onChange, ...rest }) {
   // registerParms = react hook form register 내부 Props
   // subRef = categorySelect 의 참조
+  console.log('타타타타타');
 
+  const { control } = useFormContext();
+
+  // const { ref, ...last } = register;
+
+  // console.log('나머지', rest);
   return (
     <>
-      <S.CustomSelector
-        ref={(el) => {
-          register.ref(el);
-          if (subRef) subRef.current = el;
-        }}
-        {...rest}
+      <Controller
+        control={control}
+        name={register.name}
+        render={({ field }) => (
+          <S.CustomSelector
+            ref={(el) => {
+              if (subRef) subRef.current = el;
+            }}
+            onChange={(select) => {
+              field.onChange(select);
+              onChange(select);
+            }}
+            {...rest}
+          />
+        )}
       />
     </>
   );
